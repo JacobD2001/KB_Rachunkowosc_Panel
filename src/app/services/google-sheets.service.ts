5b53a3639d484167500b444b9f40932bcada7c72
Jakub Dzikowski <dzikowskijakub2001@gmail.com>
2024-07-23 15:41:05 +0300

    env

--- src/app/services/google-sheets.service.ts
+++ src/app/services/google-sheets.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment.development';
+import { env } from 'process';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GoogleSheetsService {
+  private apiKey = environment.googleSheetsApiKey;
+  private spreadsheetId = environment.spreadSheetId;
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getSheetData(sheetName: string): Observable<any> {
+    const url = `https://sheets.googleapis.com/v4/spreadsheets/${this.spreadsheetId}/values/${sheetName}?key=${this.apiKey}`;
+    return this.http.get<any>(url);
+  }
 }
