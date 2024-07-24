24a7de1632db2794c88d6bfa9c99b2f32439b852
Jakub Dzikowski <dzikowskijakub2001@gmail.com>
2024-07-24 17:52:46 +0300

    fixed data correctly downloaded and displayed from api

--- src/app/components/panel/panel.component.ts
+++ src/app/components/panel/panel.component.ts
@@ -1,33 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 import { GoogleSheetsService } from '../../services/google-sheets.service';
 import { CommonModule } from '@angular/common';
-import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-panel',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './panel.component.html',
-  styleUrl: './panel.component.css'
+  styleUrls: ['./panel.component.css'],
 })
 export class PanelComponent implements OnInit {
-public sheetData: any[] = [];
+  private sheetName = 'test';
+  public sheetData: any[] = [];
+  public headers: string[] = [];
+  public rows: any[] = [];
 
-constructor(private googleSheetsService: GoogleSheetsService) { }
+  constructor(private googleSheetsService: GoogleSheetsService) {}
 
-ngOnInit(): void {
-  this.googleSheetsService.getSheetData('Sheet1').subscribe(data => {
-    if (data && data.values) {
-      // Assuming the first row contains headers
-      const headers = data.values[0];
-      this.sheetData = data.values.slice(1).map((row: any[]) => {
-        let obj: any = {};
-        row.forEach((cell: any, index: number) => {
-          obj[headers[index]] = cell;
-        });
-        return obj;
-      });
-    }
-  });
-}
+  ngOnInit(): void {
+    this.googleSheetsService.getSheetData(this.sheetName).subscribe((data: any) => {
+      //console.log(data);
+      if (data && data.values && data.values.length > 0) {
+        this.headers = data.values[0];
+        this.rows = data.values.slice(1);
+        //console.log('headers', this.headers);
+        //console.log('rows', this.rows);
+      }
+    });
+  }
 }
--- src/app/services/google-sheets.service.ts
+++ src/app/services/google-sheets.service.ts
@@ -1,8 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { catchError, Observable } from 'rxjs';
 import { environment } from '../../environments/environment.development';
-import { env } from 'process';
 
 @Injectable({
   providedIn: 'root'
@@ -17,4 +16,13 @@ export class GoogleSheetsService {
     const url = `https://sheets.googleapis.com/v4/spreadsheets/${this.spreadsheetId}/values/${sheetName}?key=${this.apiKey}`;
     return this.http.get<any>(url);
   }
+
+  // getSheetData(sheetName: string): Observable<any> {
+  //   const url = `https://sheets.googleapis.com/v4/spreadsheets/${this.spreadsheetId}/values/${sheetName}?key=${this.apiKey}`;
+  //   return this.http.get<any>(url).pipe(
+  //     catchError(err => {
+  //       throw new Error('Error fetching sheet data: ' + err.message);
+  //     })
+  //   );
+  // }
 }
